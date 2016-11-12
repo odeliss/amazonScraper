53b66b24393e77dd9981b8b0ad320b3313f9aa75
odeliss <olivier@wapetex.com>
2016-11-12 18:15:10 +0100

    first commit

---
+++ automatedClassification.py
@@ -0,0 +1,98 @@
+#!python3
+
+#This is a module that scan a directory of images (rawImages) and identify watches in it
+#if yes it is saved in the \watches directory with a watch_randomnb file name
+#else it is saved in the \notwatch directory
+#the model is trained with images found in \dataset directory
+#TO DO: use the descriptor object in the descriptorWatches.py file
+#TO DO: use the progressbar object (cf example in indexwatchescaltalog.py
+#TO DO: tell when finished
+
+import cv2
+from pathlib import Path
+import datetime
+from hog import HOG
+from skimage import feature
+from sklearn.ensemble import RandomForestClassifier
+
+
+class isThatAWatch:
+    def __init__(self, image):
+        self.savedWatchPath = str(directoryWatches)+"_"
+        self.savedNotWatchPath = str(directoryNotWatches)+"_"
+        #load the image and call the model to define if it is a watch or not
+        imageToRecognize=cv2.imread(image)
+        features=describe(imageToRecognize)
+        prediction=model.predict(features.reshape(1,-1))[0]
+
+        if prediction == WATCH: #save it to the watch directory
+            fileName=str(self.savedWatchPath)+"watch_"+str(datetime.datetime.now())[20:]+".jpg"
+            cv2.imwrite(fileName, imageToRecognize)
+            print("[INFO] Stored watch in: "+fileName)
+        elif prediction == NOTWATCH: #save it to the notwatch directory
+            fileName=str(self.savedNotWatchPath)+"notwatch_"+str(datetime.datetime.now())[20:]+".jpg"
+            cv2.imwrite(fileName, imageToRecognize)
+            print("[INFO] Stored not_watch in: "+fileName)
+            
+def initializeRandomForest():
+    #this function willtrain a randomforest and return the corresponding model
+    print("[INFO] extracting features...")
+    imagePaths = sorted(list(directoryDataset.glob('**/*.jpg')))
+    trainLabels=[]
+    trainData=[]
+    
+    # loop over the images in the input directory
+    for imagePath in imagePaths:
+            # extract the label and load the image from disk
+            label = str(imagePath)[str(imagePath).find("_") + 1:].split("_")[0]
+            image = cv2.imread(str(imagePath))
+            # extract features from the image, then update the list of labels and
+            # features
+            features = describe(image)
+            trainLabels.append(label)
+            trainData.append(features)
+    model = RandomForestClassifier(n_estimators=20, random_state=42)
+    # train the decision tree
+    print("[INFO] training model...")
+    model.fit(trainData, trainLabels)
+    print("[INFO] model is trained...")
+    return(model)
+            
+def describe(image):
+    
+    # extract HOG features of the image and return it
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    standardized=cv2.resize(gray, STANDARD_SIZE)
+    equalized = cv2.equalizeHist(standardized)
+    hog = feature.hog(equalized, orientations= HOG_ORIENTATIONS,
+                      pixels_per_cell= HOG_PIXELS,
+                      cells_per_block= HOG_CELLS,
+                      transform_sqrt= True)
+        
+    # return a concatenated feature vector of color statistics and Haralick
+    # texture features
+    #return np.hstack([colorStats, haralick])
+    return hog
+
+# 1.0 --- initialize some variables
+basePath="D:/Users/olivi/ComputerVision/amazonScraper"
+directoryDataset = Path(basePath+"/dataset")
+directoryWatches= Path(basePath+"/watches/_")
+directoryNotWatches= Path(basePath+"/notwatches/_")
+directoryRawImages= Path(basePath+"/rawImages")
+
+HOG_ORIENTATIONS = 14
+HOG_PIXELS = (8,8)
+HOG_CELLS = (4,4)
+STANDARD_SIZE = (190, 246) #new size of individual digit
+WATCH = 'watch'
+NOTWATCH = 'notwatch'
+model=initializeRandomForest()
+
+listOfImages=list(directoryRawImages.glob('**/*.jpg'))
+
+for filename in (listOfImages):
+    isThatAWatch(str(filename))
+    
+
+
---
+++ scrapAmazon.py
@@ -0,0 +1,160 @@
+#! python3
+#search amazon for watches and store the images found in a directory 'rawimages'
+#use machine learning to identify a watch and store the image in the dataset
+#TO DO : download page range instead of all pages
+#TO DO : rewrite with scrappy
+#UPDATE 12/11 - Added save scraped data to CSV file
+
+import requests
+import os
+import bs4
+from pathlib import Path
+from fake_useragent import UserAgent
+import time
+import cv2
+import threading
+from redis import Redis
+import csv
+
+
+#will work better when connecting on avast secure line
+#altenative is to implement a rotating public proxy
+#doc: http://www.doc.ic.ac.uk/~pg1712/blog/python-proxy/
+
+#0.0 ---- define some parameters
+#url = 'https://www.amazon.fr/s/url=search-alias%3Dwatches&field-keywords=montres'
+url = 'https://www.amazon.fr/s/url=search-alias%3Dwatches&field-keywords=citizen'
+directoryImages = Path("D:/Users/olivi/ComputerVision/amazonScraper/rawImages/_")
+NB_PAGES_TO_DOWNLOAD = 1
+REDIS_PORT = 6379
+PATH_POS = 0 #location in the redisDB of the path
+ASIN_POS = 1 #location in the redisDB of the ASIN
+
+
+#the following function get an url and return the request object
+# it encapsulate error handling when getting an URL
+# options are random useragent and timeout
+#TO DO : add possibility to call a proxy
+
+def getPageContent(url, userAgent = None, reqTimeout = 20):
+    try:
+        if userAgent == True:
+            ua= UserAgent()
+            userAgent={'User-Agent': ua.random}
+            res = requests.get(url, headers=userAgent, timeout=reqTimeout)
+        else:
+            res = requests.get(url, timeout=reqTimeout)
+
+    except ConnectionError:
+        print("Error connecting : " + str(url))
+        print("CONNECTION ABORTED")
+        pass
+    except requests.exceptions.ReadTimeout:
+        print("Read timed out after "+str(reqTimeout)+ "seconds")
+        print("CONNECTION ABORTED")
+        pass
+    return(res)
+
+def saveImageToDisk(index, soupObject, redisPipeline):
+    #Use chrome F12 - right click - copy selector on the element
+    selector = "#result_"+str(index)+" > div > div.a-row.a-spacing-base > div > div > a > img"
+    asinSelector = "#result_"+str(index)
+    asin = 'NA'
+    path = 'NA'
+    pictureUrl= ""
+
+    #Extract the ASIN
+    asin = soupObject.select(asinSelector)
+    if asin == []:
+        print("Could not find an element with asin selector." + str(asinSelector))
+    else: #get the asin
+        asin=asin[0].get('data-asin')
+
+    #Extract the image path,
+    element =  soupObject.select(selector)
+
+    if element == []:
+        print("Could not find an element with selector." + str(selector))
+    else: #get image path
+        pictureUrl = element[0].get('src')
+        print('{0} - Downloading image {1}...'.format(str(index),pictureUrl))
+        image=getPageContent(pictureUrl, True) #get the picture with random UA and 5s timeout
+        #save the images to the imagedirectory
+        pathImage = str(directoryImages)+pictureUrl[49:60]+".jpg"
+        imageFile=open(pathImage, 'wb')
+        for chunk in image.iter_content(100000):
+            imageFile.write(chunk)
+        imageFile.close()
+
+    #store the image path and corresponding ASIN in the redis pipeline
+    if asin != 'NA' and asinSelector != 'NA':
+        redisPipeline.rpush("path:{}".format(pictureUrl[49:60]), pathImage, asin)
+        #better to encode otherwize there is a 'b' coming before in the file
+        #source: http://stackoverflow.com/questions/25745053/about-char-b-prefix-in-python3-4-1-client-connect-to-redis
+
+
+def getNextPage():
+    selector = "#pagnNextLink"
+    element = soup.select(selector)
+    url = "https://www.amazon.fr"+element[0].get('href')
+    return(url)
+
+def saveScrapedPageToCSV(redisDB):
+    #source for CSV: page 322 automate boring stuff in python
+    csvFile=open(str(directoryImages)+"images.csv", 'a+', newline='') #append to the file and create it if needed
+    outputWriter = csv.writer(csvFile)
+    #loop in the redisDB and store the page scraped data in the CSV file
+    for key in redisDB.scan_iter("path:*"):
+        redisData = redisDB.lrange(key, 0, -1)
+        pathCSV = redisData[PATH_POS]
+        asinCSV = redisData[ASIN_POS]
+        outputWriter.writerow([pathCSV, asinCSV])
+        redisDB.delete(key) #delete the key now useless
+    #close the csv for this page
+    csvFile.close()
+
+
+
+for page in range (0, NB_PAGES_TO_DOWNLOAD): #download NB_PAGES_TO_DOWNLOAD pages
+#open a Redis pipeline, it will be used to store the scraped data and save to CSV
+    redisDB = Redis(host="localhost", port=REDIS_PORT, db=0, charset="utf-8", decode_responses=True)
+    #SOURCE: http://stackoverflow.com/questions/25745053/about-char-b-prefix-in-python3-4-1-client-connect-to-redis
+    
+    redisPipeline = redisDB.pipeline()
+
+    #1.1 --- Download the page ---
+    print('Downloading page %s...' %url)
+    res = getPageContent(url, True) #request url with random ua and 20s 
+    soup=bs4.BeautifulSoup(res.text, "html.parser") #create an object containing the HTML of the page
+
+    #1.2 --- Save images of this page to disk
+    #There are 50 elements per page, loop 50 triggering 50 times
+
+    saveThreads=[]
+    for i in range(0, 48):
+        index=  page * 48 + i 
+        #save the image to the disk AND save scraped data to the Redis pipeline
+        saveThread=threading.Thread(target=saveImageToDisk, args=(index, soup, redisPipeline )) 
+        saveThreads.append(saveThread)
+        saveThread.start()
+        #wait for all threads to end for this page
+    for saveThread in saveThreads:
+        saveThread.join()  
+    #execute the redis pipeline and save the data to a CSV file       
+    redisPipeline.execute()
+    redisDB.save()
+
+    saveScrapedPageToCSV(redisDB) #will also delete the rediskeys
+
+
+    print('Page ' +str(page+1)+ ' Scraped !')
+
+    #1.3 --- get next page and continue the download
+    try:
+        url=getNextPage()
+    except Exception as error:
+        print("Waiting 15s because of error: " + str(error.__doc__)+ str(error.message))
+        time.sleep(15) #wait for the connection to be back or button to appear
+        url=getNextPage()
+    
+
